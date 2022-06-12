e5132dfbb85814445421fffdd92a3f92f594a055
kkchart9 <koudai200109@gmail.com>
2022-06-12 14:44:57 +0900

    schedule.css追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/schedule.css') }}" rel="stylesheet">
 </head>
 
 <body class="h-100">
