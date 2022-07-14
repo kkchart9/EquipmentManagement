65f539cba0bec83018cd020a1960aca306b22f20
waka0613 <waka6256@gmail.com>
2022-07-15 08:51:44 +0900

    ホーム画面実装に伴う修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Schedule;
-use App\Models\Equipments;
-use App\Calendar\CalendarView;
 
 class HomeController extends Controller
 {
@@ -26,10 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $today = date('Y-m-d');
-        $schedule = new Schedule;
-        $data = $schedule->orderBy('schedule_date', 'asc')->where('schedule_date', '>=', $today)->get();
-		$calendar = new CalendarView(time());
-        return view('home.index', compact('data','calendar'));
+        return view('home');
     }
 }
