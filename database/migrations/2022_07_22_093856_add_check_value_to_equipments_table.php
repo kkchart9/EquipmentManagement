4298d81f9e843d040f0f069b9d81b58aaaeb9702
kkchart9 <koudai200109@gmail.com>
2022-07-22 18:47:59 +0900

    migration ファイル調整

--- database/migrations/2022_07_22_093856_add_check_value_to_equipments_table.php
+++ database/migrations/2022_07_22_093856_add_check_value_to_equipments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddScheduleIdToEquipmentsTable extends Migration
+class AddCheckValueToEquipmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddScheduleIdToEquipmentsTable extends Migration
     public function up()
     {
         Schema::table('equipments', function (Blueprint $table) {
-            $table->integer('schedule_id');
+            $table->integer('check_value');
         });
     }
 
@@ -26,7 +26,7 @@ class AddScheduleIdToEquipmentsTable extends Migration
     public function down()
     {
         Schema::table('equipments', function (Blueprint $table) {
-            $table->dropColumn('schedule_id');
+            //
         });
     }
 }
